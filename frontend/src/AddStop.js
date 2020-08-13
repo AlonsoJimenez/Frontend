e188c7ddf343b5a30aa3347d04a49b0ce5e4714b
Alonso <alonso.jimenez@hotmail.com>
2020-08-12 21:30:06 -0600

    Cambios en estilo grafico

--- frontend/src/AddStop.js
+++ frontend/src/AddStop.js
@@ -1,158 +1,168 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
+import './AddStop.css';
+import Container from 'react-bootstrap/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Row from 'react-bootstrap/Row'   
-import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import axios from 'axios';
 
-var select =[];
+const apiGetList = axios.create({
+  baseURL: 'http://localhost:9080/BackEnd/rest/user/stations'
+})
 
-var selectTo =[];
+var select = [];
 
-const options = [createData("tres rios"),createData("cartago"), createData("sj")];
+var selectTo = [];
 
-const optionsTo = [createData("tres rios"),createData("cartago"), createData("sj")];
+export default class AddStop extends React.Component {
 
-function createData(name){
-    return {name}
-}
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(3),
-    flexDirection: 'column',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    alignItems: 'center',
-    width: '85%',
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    alignItems: 'center',
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function AddStop() {
   
-  const classes = useStyles();
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
+  render() {
+    
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className = "AddStop">
           <Container>
-           <Typography component="h1" variant="h3" >
-                        Railspot
+            <Typography component="h1" variant="h3" >
+              Railspot
            </Typography>
           </Container>
-        <Typography component="h1" variant="h4">
-          Agregar parada
+          <Typography component="h1" variant="h4">
+            Agregar parada
         </Typography>
-        <form className={classes.paper} noValidate>
-          <MenuFrom/>  
-          <MenuTo/>     
-        </form>
-      </div>
-    </Container>
+          
+            <div>
+            <MenuFrom/>
+            </div>
+            <div>
+            <MenuTo/>
+            </div>
+          
+        </div>
+      </Container>
 
-  );
+    );
+  }
 
 }
 
-function MenuTo(){
-  
-    const [anchorEl, setAnchorEl] = React.useState(null);    
-    const handleClick = (event) => {
-      
-      setAnchorEl(event.currentTarget);
-    };
-    
-    const handleClose = (name) => {
-      selectTo = [name]
-      console.log(selectTo);
-      setAnchorEl(null);
-    };
-      return(
-              <Container>
-                <Row>
-                      <Col>
-                          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                            Estacion
-                          </Button>
-                          <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                          >
-                          {optionsTo.map((option) => (
-                            <MenuItem onClick={() =>handleClose(option.name)} key={option.name} >{option.name}</MenuItem>
-                          ))}
-                          </Menu>
-                      </Col>
-                </Row>
-                <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-            >
-              Aceptar
-            </Button>
-            </Container> 
-      )
+class MenuTo extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      optionsTo: [],
+      anchorEl: null 
+    }
+    apiGetList.get('/').then(res => {
+      console.log("listo")
+      console.log(res.data)
+    })
   }
 
-function MenuFrom(){
-  
-  const [anchorEl, setAnchorEl] = React.useState(null);    
+  render(){
   const handleClick = (event) => {
     
-    setAnchorEl(event.currentTarget);
+    this.setState({anchorEl:event.currentTarget });
   };
-  
+
+  const handleClose = (name) => {
+    selectTo = [name]
+    console.log(selectTo);
+    this.setState({anchorEl: null});
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            Estacion
+                          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={this.state.anchorEl}
+            open={Boolean(this.state.anchorEl)}
+            onClose={handleClose}
+          >
+            {this.state.optionsTo.map((option) => (
+              <MenuItem onClick={() => handleClose(option.name)} key={option.name} >{option.name}</MenuItem>
+            ))}
+          </Menu>
+        </Col>
+      </Row>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+      >
+        Aceptar
+            </Button>
+    </Container>
+  )
+            }
+}
+
+class MenuFrom extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      options: [],
+      anchorEl: null 
+    }
+    apiGetList.get('/').then(res => {
+      this.setState({options: res.data})
+    })
+  }
+
+  render(){
+  const handleClick = (event) => {
+    
+    this.setState({anchorEl:event.currentTarget });
+  };
+
   const handleClose = (name) => {
     select = [name]
     console.log(select);
-    setAnchorEl(null);
+    this.setState({anchorEl: null});
   };
-    return(
-            <Container>
-              <Row>
-                  <Col><Typography component="h1" variant="h6">
-                       De
+  return (
+    <Container>
+      <Row>
+        <Col><Typography component="h1" variant="h6">
+          De
                     </Typography></Col>
-                    <Col>
-                        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                          Estacion
+        <Col>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            Estacion
                         </Button>
-                        <Menu
-                          id="simple-menu"
-                          anchorEl={anchorEl}
-                          open={Boolean(anchorEl)}
-                          onClose={handleClose}
-                        >
-                        {options.map((option) => (
-                          <MenuItem onClick={() =>handleClose(option.name)} key={option.name} >{option.name}</MenuItem>
-                        ))}
-                        </Menu>
-                    </Col>
-                    <Col><Typography component="h1" variant="h6">
-                      Hasta
+          <Menu
+            id="simple-menu"
+            anchorEl={this.state.anchorEl}
+            open={Boolean(this.state.anchorEl)}
+            onClose={handleClose}
+          >
+            {this.state.options.map((option) => 
+              <MenuItem onClick={() => handleClose(option.station.name)} key={option.station.name} >{option.station.name}</MenuItem>
+            )}
+          </Menu>
+        </Col>
+        <Col><Typography component="h1" variant="h6">
+          Hasta
                     </Typography></Col>
-              </Row>
-              
-          </Container> 
-    )
+      </Row>
+
+    </Container>
+  )
+            }
 }
